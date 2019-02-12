2d87d0b471b654fd0c320d1499416313704c024e
Marcus Ames <marcusames00@gmail.com>
2019-02-11 17:52:07 -0700

    small fix

--- front-end/src/store.js
+++ front-end/src/store.js
@@ -1,6 +1,6 @@
 import { createStore, applyMiddleware } from 'redux';
 import rootReducer from './reducers/rootReducer';
-import thunk from 'react-redux';
+import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
 
 const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
