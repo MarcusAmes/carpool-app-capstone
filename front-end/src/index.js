01bb66eef588ebb62167facc1ad2ae904dbcd1de
Marcus Ames <marcusames00@gmail.com>
2019-02-11 17:44:25 -0700

    store boilerplate

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -1,5 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom'
+import store from './store'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <BrowserRouter>   
+      <App /> 
+    </BrowserRouter>
+  </Provider>
+, document.getElementById('root'));
